81a507354addf2c2a23f7278812d83dc7eaa29d1
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-05-09 14:36:41 +0530

    array method part I

---
+++ 03_array_method/index.js
@@ -0,0 +1,80 @@
+// Array: An array is a special variable, which can hold more than one value
+
+// using array literal
+const fruits = ["Apple", "Banana", "Mango"]
+//using new keyword
+
+const fruits2 = new Array("Apple", "Banana", "Mango")
+
+// array to string 
+
+fruits.toString();
+
+// difference between Arrays and Objects
+
+// In js, arrays use numbered indexes
+// where as object use named indexes
+
+// Array are a special kind of objects, with numbered indexes
+
+
+// BASIC ARRAY METHOD 
+
+// array length ()  ---DONE
+// array toString() ---
+// array at()
+// array join()
+// array pop ()
+// array push ()
+// array shift() 
+// array unshift()
+// array delete()
+// array concat ()
+// array copyWith()
+// array flat ()
+// array splice ()
+// array toSplice()
+// array slice ()
+
+let arr = [1,2, 3,4, 5, 5, 6, 7,8]
+
+// length method : its give the length of array
+let len =arr.length
+// console.log(len)
+
+//toString() method : converts an array to a string of (comma seprated values.)
+
+let arrToString = arr.toString()
+
+// console.log(arrToString, arr);
+
+// at () method: es2022 introduced the array method at() 
+// The at() method returns an indexed element from an array.
+
+let secondElement = arr.at(1)
+// console.log(secondElement);
+
+// join() method : The join method joins all array elements into a string. 
+// it behaves just like toString(), but in addition you can specify the separator:
+
+
+const names = ['mohan','sohan','rohan','mahesh','suresh']
+let joinNames = names.join(' ')
+// console.log(joinNames)
+
+//push and pop method
+
+//pop() method removes the last element form an array
+// and returns the value that was popped out
+
+let poppedOutElement = names.pop()
+// console.log(`popped out element: ${poppedOutElement} original array: ${names}`)
+
+//push method adds a new element to an array (at the end):
+// and returns the new array length 
+
+
+console.log(names)
+let lenOfNewArray = names.push("magaru")
+console.log(lenOfNewArray)
+console.log(names)
